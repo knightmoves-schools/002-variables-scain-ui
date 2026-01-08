032fb254f8febe4bb1d6d64c9acdc163e2720b15
scain-ui <scain@midland.k12.ia.us>
2026-01-08 18:07:23 +0000

    1234567890

--- Course.cs
+++ Course.cs
@@ -2,7 +2,7 @@
 public class Course
 {   
     public int MaxStudents = 25;
-    public int perfectScore = 100;
+    public const int perfectScore = 100;
     
     public int CourseLength(){
         int courseLength = 20;
