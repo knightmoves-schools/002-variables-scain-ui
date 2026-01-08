efb663248291e8a5f6926bafe8c284c6366765f2
scain-ui <scain@midland.k12.ia.us>
2026-01-08 18:06:24 +0000

    1234567890

--- Course.cs
+++ Course.cs
@@ -1,8 +1,8 @@
 ﻿namespace knightmoves;
 public class Course
 {   
-    public int MaxStudents { get; set; } = 25;
-    public int perfectScore { get; set; } = 100;
+    public int MaxStudents = 25;
+    public int perfectScore = 100;
     
     public int CourseLength(){
         int courseLength = 20;
